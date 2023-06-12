6c56910398cd38043323d4bfab24100a9cac3432
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-06-12 18:10:31 -0300

    Validation

--- src/main/java/com/jlucas/dscommerce/controllers/ProductController.java
+++ src/main/java/com/jlucas/dscommerce/controllers/ProductController.java
@@ -19,6 +19,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.jlucas.dscommerce.dto.ProductDTO;
 import com.jlucas.dscommerce.services.ProductService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
@@ -47,7 +49,7 @@ public class ProductController {
 	
 	//201
 	@PostMapping()
-	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
 		dto =  service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}")
 				.buildAndExpand(dto.getId()).toUri();
@@ -55,7 +57,7 @@ public class ProductController {
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id,@Valid @RequestBody ProductDTO dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok(dto);
 	}
