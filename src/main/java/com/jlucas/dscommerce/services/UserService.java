50d02ff6e391128eca4ade25eed8fadb94d8891a
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-07-04 15:16:06 -0300

    Get User Logged

--- src/main/java/com/jlucas/dscommerce/services/UserService.java
+++ src/main/java/com/jlucas/dscommerce/services/UserService.java
@@ -3,11 +3,16 @@ package com.jlucas.dscommerce.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.jlucas.dscommerce.dto.UserDTO;
 import com.jlucas.dscommerce.entities.Role;
 import com.jlucas.dscommerce.entities.User;
 import com.jlucas.dscommerce.projections.UserDetailsProjection;
@@ -25,7 +30,7 @@ public class UserService implements UserDetailsService {
 		List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
 		
 		if(result.size() == 0) {
-			throw new UsernameNotFoundException("User not found");
+			throw new UsernameNotFoundException("Email not found");
 		}
 		
 		User user = new User();
@@ -35,6 +40,26 @@ public class UserService implements UserDetailsService {
 			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
 		}
 		return user;
+	}
+	
+	protected User authenticated() {
+		
+		try {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+			String username = jwtPrincipal.getClaim("username");
+			
+			return repository.findByEmail(username).get();
+		} catch(Exception e) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		
+	}
+	
+	@Transactional(readOnly = true)
+	public UserDTO getMe() {
+		User user = authenticated();
+		return new UserDTO(user);
 	}
 
 }
